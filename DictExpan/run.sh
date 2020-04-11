132c6c505855b9e17dbc98bb236f2c8aea8df372
Xuan Wang <xuanwang@Xuans-MacBook-Pro.local>
2020-04-10 20:44:54 -0500

    DictExpan README

--- DictExpan/run.sh
+++ DictExpan/run.sh
@@ -1,7 +1,8 @@
-# input: a tokenized corpus. e.g., data/bc5/input_text.txt; a core dictionary, e.g., data/bc5/dict_core.txt; a full dictionary, e.g., data/bc5/dict_full.txt
-# output: expanded dictionaries. e.g., data/bc5/dict_core_expand.txt, data/bc5/dict_core_full.txt
+# Input: a tokenized corpus. e.g., data/bc5/input_text.txt; a core dictionary, e.g., data/bc5/dict_core.txt; a full dictionary, e.g., data/bc5/dict_full.txt
+# Output: expanded dictionaries. e.g., data/bc5/dict_core_expand.txt, data/bc5/dict_core_full.txt
 
-# need to start CoreNLP
+# Need to start CoreNLP
+# Download the Stanford CoreNLP Toolkit to src/tools/CoreNLP/
 # cd src/tools/CoreNLP/stanford-corenlp
 # java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
 
